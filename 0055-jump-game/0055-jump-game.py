4a24eb9446265d2519c61c0e56177394472af5ae
Rutuja Sonawane <59499279+Rutujas1709@users.noreply.github.com>
2023-08-30 21:17:57 -0700

    Time: 401 ms (83.01%), Space: 17.5 MB (90.09%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        size = len(nums) - 1
+        maxjump = 0
+        ins = nums[0]
+        for i in range(len(nums))[::-1]:
+            if i + nums[i] >= size:
+                size = i
+        return not size        
+        
